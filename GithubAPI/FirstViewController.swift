52e3a71c388210c458944776e1e176b4668a85c5
祖父江亮 <sn0wbreeze2000@yahoo.co.jp>
2017-02-13 12:37:52 +0900

    追加：Alamofire実装、仮動作確認

--- GithubAPI/FirstViewController.swift
+++ GithubAPI/FirstViewController.swift
@@ -7,19 +7,43 @@
 //
 
 import UIKit
+import Alamofire
 
 class FirstViewController: UIViewController {
+    
+    @IBOutlet weak var searchWord: UITextField!
 
+    @IBAction func buttonPushed(_ sender: Any) {
+        print("%@",self.searchWord.text as Any)
+        
+        let gs = githubSearch()
+        gs.getData()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+
     }
 
 
 }
 
+class githubSearch{
+    public func getData(reset:Bool = false) {
+        
+        let parameters: Parameters = ["q":"OthloTech","sort":"stars","order":"desc"]
+        Alamofire.request("https://api.github.com/search/repositories", method: .get, parameters: parameters).responseJSON{
+            response in
+            
+            if let JSON = response.result.value {
+                print("JSON: \(JSON)")
+            }
+        }
+    }
+    
+}
---
+++ Podfile
@@ -0,0 +1,20 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'GithubAPI' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for GithubAPI
+	pod 'Alamofire', :git => 'https://github.com/Alamofire/Alamofire.git'
+  target 'GithubAPITests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'GithubAPIUITests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
